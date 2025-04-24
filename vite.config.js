3f3abf99f219fbe51fadadc30abb9ec22077b90f
Dhananjai Pratap Singh <143176852+DhananjaiPS@users.noreply.github.com>
2025-04-24 21:32:20 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+// ESM-compatible __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  root: path.join(__dirname, 'src'),
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // now this works!
+    },
+  },
+});
